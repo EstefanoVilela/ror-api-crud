a437233dc07ad599bd2cd8bc87d231b35da8b3b7
EstefanoVilela-Geosatelital <evilela@geosatelital.com.pe>
2024-02-14 18:59:35 -0500

    cors, rescues and validates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from Exception, with: :handle
+
+  private
+    def handle ex
+      render json: { message: 'Error!' }, status: :unprocessable_entity
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,3 @@
 class Employee < ApplicationRecord
+  validates_presence_of(:last_name, :first_name, :age)
 end
