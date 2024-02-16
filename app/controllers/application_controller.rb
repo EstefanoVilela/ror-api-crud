3ff7c204856483128ef757c464b30a50de62ed96
EstefanoVilela-Geosatelital <evilela@geosatelital.com.pe>
2024-02-15 22:17:41 -0500

    add token validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :valid_token
   rescue_from Exception, with: :handle
 
   private
+    def valid_token
+      raise 'Token inválido' unless request.headers[:Authorization] === 'abc123'
+    end
+
     def handle ex
-      render json: { message: 'Error!' }, status: :unprocessable_entity
+      render json: { message: ex }, status: :unprocessable_entity
     end
 end
